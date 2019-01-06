91661d83b59644134a5b6491549196851d0ee7f6
patrikravnak <patrik.ravnak@gmail.com>
2019-01-06 16:49:13 +0100

    Povezava dodana

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser Middleware
@@ -16,6 +18,8 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+  // Uporabi routes
+  app.use('./api/items', items )
 
   const port = process.env.PORT || 5000;
 
